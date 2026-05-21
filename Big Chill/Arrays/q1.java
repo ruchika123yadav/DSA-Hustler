d4138da7b7a01533f4486e7aed7dd2e2e61e2112
Thirukumaran <114419114+Thiru-kumaran-R@users.noreply.github.com>
2026-05-21 21:13:20 +0530

    Completed first video in Strivers

--- Big Chill/Arrays/q1.java
+++ Big Chill/Arrays/q1.java
@@ -68,3 +68,60 @@ public class q1 {
         System.out.println(secondMax);
     }
 }
+
+// Check if the array is sorted or not
+// Optimal 
+
+class q1{
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5};
+        int size = arr.length;
+
+        for(int i = 0; i < size - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                System.out.println("Not Sorted");
+                return;
+            }
+        }
+        System.out.println("Sorted");
+    }
+}
+
+// Remove duplicates from a sorted array and return the new length of the array.
+//Brute Force
+
+class q1{
+    public static void main(String[] args){
+        HashSet<Integer> set = new HashSet<>();
+        int[] arr = {1, 1, 2, 2, 3, 4, 5};
+        int size = arr.length;
+
+        for(int i = 0; i < size; i++){
+            set.add(arr[i]);
+        }
+
+        for(int i = 0; i < set.size(); i++){
+            arr[i] = set.get(i);
+        }
+
+        System.out.println(set.size());
+    
+    }
+}
+
+// Optimal Solution
+class q1{
+    public static void main(String[] args){
+        int[] arr = {1, 1, 2, 2, 3, 4, 5};
+        int size = arr.length;
+        int pointer = 0;
+
+        for(int i = 1; i < size; i++){
+            if(arr[pointer] != arr[i]){
+                arr[pointer + 1] = arr[i];
+                pointer += 1;
+            }
+        }
+        System.out.println(pointer + 1);
+    }
+}
