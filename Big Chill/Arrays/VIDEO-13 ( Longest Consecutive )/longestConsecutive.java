b9584a64439328715c06683f49a1c37879f2d441
Thirukumaran <114419114+Thiru-kumaran-R@users.noreply.github.com>
2026-06-02 21:49:05 +0530

    Update longestConsecutive.java

--- Big Chill/Arrays/VIDEO-13 ( Longest Consecutive )/longestConsecutive.java
+++ Big Chill/Arrays/VIDEO-13 ( Longest Consecutive )/longestConsecutive.java
@@ -23,6 +23,39 @@ class Solution {
     }
 }
 
+// Better
+import java.util.Arrays;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        if (nums.length == 0)
+            return 0;
+
+        Arrays.sort(nums);
+
+        int longest = 1;
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] == nums[i - 1]) {
+                continue; // skip duplicates
+            }
+
+            if (nums[i] == nums[i - 1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            longest = Math.max(longest, count);
+        }
+
+        return longest;
+    }
+}
+
 // Optimal
 import java.util.*;
 
