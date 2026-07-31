68240e4d20711e3a2824ab2c25f7b7657b304b2b
Ruchika Yadav <145842561+ruchika123yadav@users.noreply.github.com>
2026-07-31 19:48:43 +0530

    done potd

--- Rath/POTD/potd.py
+++ Rath/POTD/potd.py
@@ -550,3 +550,26 @@ class Solution:
         return cost
 
         
+
+# MINIMUM NUMBER OF PUSHES TO TYPE WORD ||->https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/description/?envType=daily-question&envId=2026-07-31
+
+        class Solution:
+    def minimumPushes(self, word: str) -> int:
+        freq = [0] * 26
+
+        for ch in word:
+            freq[ord(ch) - ord('a')] += 1
+
+        freq.sort(reverse=True)
+
+        ans = 0
+
+        for i in range(26):
+            if freq[i] == 0:
+                break
+
+            pushes = (i // 8) + 1
+
+            ans += freq[i] * pushes
+
+        return ans
